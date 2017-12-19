8d46ea297017ea58b2a9b018c2d694050e8a3fa9
konstantindenerz <konstantin.denerz@gmail.com>
2017-12-19 07:36:58 +0100

    Theme support for components with encapsulation.

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 
-@Component({ template: 'Child Component', styleUrls: ['./child.component.less'] })
+@Component({
+  template: 'Child Component',
+  styleUrls: ['./child.component.bar.less', './child.component.foo.less'],
+})
 export class ChildComponent {
 
 }
